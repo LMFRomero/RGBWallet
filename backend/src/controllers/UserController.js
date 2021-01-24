81423d5565188d709cc348f545b420d34a285e08
Lucas <lucasmfromero@gmail.com>
2021-01-24 10:38:44 -0300

    Added destroy in UserController

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const { SafeCreateObj } = require('../services/safeExec');
+const { SafeCreateObj, SafeFindById } = require('../services/safeExec');
 
 module.exports = {
     async store (req, res) {
@@ -16,6 +16,28 @@ module.exports = {
         
         if (!user) {
             console.log("User not created");
+            return res.status(500).end();
+        }
+
+        return res.status(201).end();
+    },
+
+    async destroy (req, res) {
+        if (!req.params.id) {
+            return res.status(400).end();
+        }
+
+        let user = await SafeFindById(User, req.params.id);
+        if (!user) {
+            return res.status(400).end();
+        }
+
+        user.isDeleted = true;
+        
+        try {
+            await user.save()
+        } catch (error) {
+            console.log(error);
             return res.status(500).end();
         }
 
