9014dcb43a71d15beb57448df9206838c1f8632e
Lucas <lucasmfromero@gmail.com>
2021-01-24 10:46:49 -0300

    Added update to UserController

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -42,5 +42,44 @@ module.exports = {
         }
 
         return res.status(200).end();
-    }
+    },
+
+    async update (req, res) {
+        if (!req.params.id) {
+            return res.status(400).end();
+        }
+
+        let { name, username, hasSold, isInProject, weeksDone, isAdmin } = req.body;
+
+        let user = await SafeFindById(User, req.params.id);
+        if (!user) {
+            return res.status(400).end();
+        }
+        
+        if (name != null) {
+            user.name = name;
+        }
+        if (username != null) {
+            user.username = name;
+        }
+        if (hasSold != null) {
+            user.hasSold = hasSold;
+        }
+        if (isInProject != null) {
+            user.isInProject = isInProject;
+        }
+        if (weeksDone != null) {
+            user.weeksDone = weeksDone;
+        }
+        if (isAdmin != null) {
+            user.isAdmin = isAdmin;
+        }
+
+        try {
+            await user.save();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).end();
+        }
+    },
 }
