364f98e3a666790f864f8cdfd154f172d7d23292
Lucas <lucasmfromero@gmail.com>
2021-02-13 22:41:09 -0300

    Base frontend

---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import MainHome from './pages/MainHome/MainHome';
+import AdmLogin from './pages/Login/AdmLogin';
+import UserLogin from './pages/Login/UserLogin';
+import UserHome from './pages/UserHome/UserHome';
+
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={MainHome} />
+                <Route path="/admLogin" exact component={AdmLogin} />
+                <Route path="/userLogin" exact component={UserLogin} />
+                <Route path="/userHome" exact component={UserHome} />
+
+            </Switch>
+        </BrowserRouter>
+    );
+}
